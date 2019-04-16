08372bc20b762e9540b0c0c6065a5bf40d3af67f
abir <abirchandrabhushan@gmail.com>
2019-04-16 19:57:22 +0100

    created initial object structure

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-//
+/**
+ * Every object has the structure:
+ * object {
+ *   mass: float,
+ *   radius: float,
+ *   velocity: [x, y],
+ *   force: [x, y],
+ *   coord: [x, y],
+ *   lastTimeUpdated: Int (Date.now())
+ *   float  ::= 64-bit Floating Point number
+ *   [x, y] ::= 2-tuple
+ * }
+ *
+ * The reason for using lists is in case I expand it to more dimensions
+ */
